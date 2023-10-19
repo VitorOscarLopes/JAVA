ea9acedd6bf6edf6871d5ebc2b85464cbe363eb0
Vitor Oscar Lopes da Fonseca <devitor2003@gmail.com>
2023-10-19 19:49:17 -0300

    Estudos Java

---
+++ Testes/CursoJava/src/testeVariaveis/TesteVariaveis.java
@@ -0,0 +1,18 @@
+package testeVariaveis;
+
+public class TesteVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Testando Condicionais.");
+		
+		int idade = 20;
+		if (idade >= 18) {
+			System.out.println("Você já tem idade para dirigir.");
+			System.out.println("Dirija com cuidado!");
+		} else {
+			System.out.println("Você ainda é menor de idade, portanto, ainda não pode dirigir.");
+		}
+		
+	}
+
+}

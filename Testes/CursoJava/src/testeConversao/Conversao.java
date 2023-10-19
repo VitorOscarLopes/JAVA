26747f69a8af4ddfee7b7b508663acd85f70d723
Vitor Oscar Lopes da Fonseca <devitor2003@gmail.com>
2023-10-19 19:00:55 -0300

    Estudos de Java
    Estudos iniciais em Java

---
+++ Testes/CursoJava/src/testeConversao/Conversao.java
@@ -0,0 +1,10 @@
+package testeConversao;
+
+public class Conversao {
+	public static void main(String[] args) {
+		//System.out.println(5.0/2);
+		double nmr = 32.3;
+		int nmr1 = (int) nmr;
+		System.out.println(nmr1);
+	}
+}
---
+++ Testes/TestVariaveisEFluxo/src/variaveis/Inteiros.java
@@ -0,0 +1,12 @@
+package variaveis;
+
+public class Inteiros {
+	public static void main(String[] args) {
+		var vini = "vinicius";
+				System.out.println(vini);
+		int idade = 17;
+		System.out.println(vini+" tem "+idade+" anos de idade.");
+		double altura = 1.84;
+		System.out.println(vini+" tem "+altura+"m de Altura.");
+	}
+}

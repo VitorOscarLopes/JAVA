633c133ffd9c1e1bb32bec7733b0d5967a0eac5a
Vitor Oscar Lopes da Fonseca <devitor2003@gmail.com>
2023-10-25 13:42:59 -0300

    Update Scanzin.java

--- exercicios/ex005/Scanzin.java
+++ exercicios/ex005/Scanzin.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Scanzin{
-    Scanner scan = new Scanner(System.in);
-    int n1,n2,res;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n1,n2,res;
+        
+        System.out.println("Digite um número:");
+        n1 = scan.nextInt();
+        System.out.println("Digite outro número:");
+        n2 = scan.nextInt();
+        res=n1+n2;
+        System.out.printf("O resultado da soma de %d e %d vale %d",n1,n2,res);
+        
+    }
 }
